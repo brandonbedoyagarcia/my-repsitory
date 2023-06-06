9660c443109011ebe13b5e6137c466841ec55ef5
brandonbedoyagarcia <135679492+brandonbedoyagarcia@users.noreply.github.com>
2023-06-05 21:41:21 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+//determinacion de variable
+float numero;
+cout<<"introduzca un numero positivo:";
+  cin>> numero;
+  //evaluando condicion
+  if(numero>0)
+    cout<< numero << " es maoyor a cero" <<endl;
+
+}
